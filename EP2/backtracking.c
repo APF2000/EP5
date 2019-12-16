c1dc5a632d6c694e1c86b380fe661b6c6bc2c2e2
APF2000 <arthurpfonseca@gmail.com>
2019-12-15 21:19:16 -0300

    Dando certo as rainhas

--- EP2/backtracking.c
+++ EP2/backtracking.c
@@ -14,31 +14,59 @@ void printMatrix(int **matrix, int lines, int cols){
   printf("\n\n");
 }
 
+int inBounds(int verified, int n){
+  if(verified >= n || verified < 0)
+    return 0;
+  return 1;
+}
+
 int isSafe(int **board, int n, int line, int col){
   int i, auxLine, auxCol;
 
-  printf("\nIs it safe?\n");
+  /*printf("\nIs it safe?\n");*/
 
   for(i = 0; i < n; i++)
     if(board[i][col] == 1 || board[line][i] == 1){
-      printf("\nNot safe1\n");
+      /*printf("\nNot safe1\n");*/
       return 0;
     }
 
   for(i = 0; i < n; i++){
-    auxLine = (line + n + i) % n;
-    auxCol  = (col  + n + i) % n;
-    printf("\nauxline=%d, auxcol=%d", auxLine, auxCol);
-    if(board[auxLine][auxCol] == 1 || board[auxLine][auxCol] == 1){
-      printf("\nNot safe2\n");
-      return 0;
+    auxLine = line  + i;
+    auxCol  = col   + i;
+    if(inBounds(auxLine, n) && inBounds(auxCol, n)){
+      /*printf("\nauxline=%d, auxcol=%d", auxLine, auxCol);*/
+      if(board[auxLine][auxCol] == 1 || board[auxLine][auxCol] == 1){
+        /*printf("\nNot safe2\n");*/
+        return 0;
+      }
     }
-    auxLine = (line + n - i) % n;
-    auxCol  = (col  + n - i) % n;
-    printf("\nauxline=%d, auxcol=%d", auxLine, auxCol);
-    if(board[auxLine][auxCol] == 1 || board[auxLine][auxCol] == 1){
-      printf("\nNot safe3\n");
-      return 0;
+    auxLine = line - i;
+    auxCol  = col  - i;
+    if(inBounds(auxLine, n) && inBounds(auxCol, n)){
+      /*printf("\nauxline=%d, auxcol=%d", auxLine, auxCol);*/
+      if(board[auxLine][auxCol] == 1 || board[auxLine][auxCol] == 1){
+        /*printf("\nNot safe3\n");*/
+        return 0;
+      }
+    }
+    auxLine = line  + i;
+    auxCol  = col   - i;
+    if(inBounds(auxLine, n) && inBounds(auxCol, n)){
+      /*printf("\nauxline=%d, auxcol=%d", auxLine, auxCol);*/
+      if(board[auxLine][auxCol] == 1 || board[auxLine][auxCol] == 1){
+        /*printf("\nNot safe4\n");*/
+        return 0;
+      }
+    }
+    auxLine = line - i;
+    auxCol  = col  + i;
+    if(inBounds(auxLine, n) && inBounds(auxCol, n)){
+      /*printf("\nauxline=%d, auxcol=%d", auxLine, auxCol);*/
+      if(board[auxLine][auxCol] == 1 || board[auxLine][auxCol] == 1){
+        /*printf("\nNot safe5\n");*/
+        return 0;
+      }
     }
   }
   return 1;
@@ -54,7 +82,7 @@ int solveNQUtil(int **board, int n, int col){
   for(i = 0; i < n; i++){
     if(isSafe(board, n, i, col)){
       board[i][col] = 1;
-      printMatrix(board, n, n);
+      //printMatrix(board, n, n);
 
       if(solveNQUtil(board, n, col+1))
         return 1;
@@ -70,14 +98,15 @@ int main(){
   int **board;
   int i, j;
 
-  for(j = 3; j < 4; j++){
-    printf("\n------------------------\n");
+  for(j = 0; j < 20; j++){
+    /*printf("\n------------------------\n");*/
     board = malloc(j * sizeof(int *));
     for(i = 0; i < j; i++){
       board[i] = malloc(j * sizeof(int));
     }
+    printf("\n%d %s", j, solveNQUtil(board, j, 0) ? "true" : "false");
 
-    printf("\n%d %s\n", j, solveNQUtil(board, j, 0) ? "true" : "false");
+    printMatrix(board, j, j);
   }
 
   return 0;

10adc53fa371e442be646b2cc54ace7067cb5399
APF2000 <arthurpfonseca@gmail.com>
2019-12-15 23:14:44 -0300

    Pilhas funcionando

--- EP2/backtracking.c
+++ EP2/backtracking.c
@@ -2,6 +2,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+  int line;
+  int col;
+} queen;
+
+typedef struct {
+  queen* v;
+  int top;
+  int max;
+} stack;
+
+stack* createStack(int n){
+  stack* aux = malloc(n * sizeof(stack));
+
+  aux->v = malloc(n * sizeof(queen));
+  aux->top = 0;
+  aux->max = n;
+  return aux;
+}
+
+void destroyStack(stack* P){
+  free(P->v);
+  free(P);
+}
+
+void resize(stack* P){
+  int i;
+  queen * aux = P->v;
+  P->v = malloc (2*P->max*sizeof(queen));
+  for (i = 0; i < P->top; i++)
+    P->v[i] = aux[i];
+  P->max = 2 * P->max;
+  free(aux);
+}
+
+void stackUp(stack* P, queen q){
+  if (P->top == P->max) resize(P);
+  printf("Stacking up (%d, %d)\n", q.line, q.col);
+  P->v[P->top] = q;
+  P->top ++;
+}
+
+int isEmpty(stack* P){
+  return (P->top == 0);
+}
+
+void unstack(stack* P){
+  if (isEmpty(P)) printf("ERROR");
+  P->top --;
+}
+
+queen top(stack* P){
+  if (isEmpty(P)) printf("ERRO");
+  return (P->v[P->top - 1]);
+}
+
+void printStack(stack* st){
+  int i;
+  printf("\n\nStack:");
+  for(i = 0; i < st->top; i++){
+    printf("\n(%d,%d)", st->v[st->top - i - 1].line, st->v[st->top - i - 1].col);
+  }
+}
+
+//////////////////////////////////////////////
+
 void printMatrix(int **matrix, int lines, int cols){
   int i, j;
   printf("\n\nPrintingmatrix\n\n");
@@ -93,20 +159,54 @@ int solveNQUtil(int **board, int n, int col){
   return 0;
 }
 
+int backTrack(int **board, int n){
+  stack* st = createStack(n);
+  int i;
+  queen teste1, teste2, teste3, teste4;
+  teste1.line = 1;
+  teste1.col = 2;
+  teste2.line = 3;
+  teste2.col = 4;
+  teste3.line = 5;
+  teste3.col = 6;
+  teste4.line = 7;
+  teste4.col = 8;
+
+  stackUp(st, teste1);
+  printStack(st);
+  stackUp(st, teste2);
+  printStack(st);
+  stackUp(st, teste3);
+  printStack(st);
+  for(i = 0; i < 1; i++){
+    stackUp(st, teste4);
+  }
+  printStack(st);
+
+  unstack(st);
+  printStack(st);
+  unstack(st);
+  unstack(st);
+  printf("\nEmpty:%s", isEmpty(st)?"true":"false");
+  unstack(st);
+  printf("\nEmpty:%s", isEmpty(st)?"true":"false");
+  return 1;
+}
+
 int main(){
 
   int **board;
   int i, j;
 
-  for(j = 0; j < 28; j++){
+  for(j = 4; j < 5; j++){
     /*printf("\n------------------------\n");*/
     board = malloc(j * sizeof(int *));
     for(i = 0; i < j; i++){
       board[i] = malloc(j * sizeof(int));
     }
-    printf("\n%d %s", j, solveNQUtil(board, j, 0) ? "true" : "false");
-
-    printMatrix(board, j, j);
+    /*printf("\n%d %s", j, solveNQUtil(board, j, 0) ? "true" : "false");*/
+    backTrack(board, j);
+    //printMatrix(board, j, j);
   }
 
   return 0;
